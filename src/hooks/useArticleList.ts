dc14d8ed1eaa6c75a33463622e670edb14be4082
ModestFun <wangziyang@uinnova.com>
2021-01-03 20:54:32 +0800

    fix: 'fix date format'

--- src/hooks/useArticleList.ts
+++ src/hooks/useArticleList.ts
@@ -5,9 +5,17 @@ async function getArticleList() {
   const result = await api.ArticleApi.getArticleList()
   return result.data
 }
+
+function fixDate(arr: any[]) {
+  return arr.map((i: { date: string; }) => {
+    const date = i.date.split('.')
+    i.date = date[0] + "-" + date[1] + "-" + date[2] + " " + date[3] + ":" + date[4]
+    return i;
+  })
+}
+
 async function useArticleList() {
-  const result = await getArticleList()
-  return reactive(result)
+  return reactive(fixDate(await getArticleList()))
 }
 
 export default useArticleList
